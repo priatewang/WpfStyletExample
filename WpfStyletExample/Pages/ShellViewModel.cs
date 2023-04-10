a6ba6fbec3d4a16e5487cdbd7d4df37b907d8e70
wangqiang <wqiang1956@126.com>
2023-04-10 16:55:04 +0800

    基础用法示例

--- WpfStyletExample/Pages/ShellViewModel.cs
+++ WpfStyletExample/Pages/ShellViewModel.cs
@@ -1,9 +1,41 @@
 ﻿using System;
 using Stylet;
+using StyletIoC;
 
 namespace WpfStyletExample.Pages
 {
     public class ShellViewModel : Screen
     {
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                SetAndNotify(ref _title, value);
+            }
+        }
+        [Inject]
+        public UserPageViewModel UserPage { get; private set; }
+
+
+        public void ChangeTitle()
+        {
+
+            Title = "使用Stylet Command改变标题";
+        }
+
+
+        public ShellViewModel()
+        {
+            Title = "Stylet MVVM 框架学习";
+            //UserPage = userPage;
+        }
+
+
     }
 }
---
+++ WpfStyletExample/Pages/UserPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfStyletExample.Pages
+{
+    public class UserPageViewModel : Screen
+    {
+        private string _actualName;
+
+        public string Name { get; set; }
+        public string ActualName
+        {
+            get
+            {
+                return _actualName;
+            }
+            set
+            {
+                SetAndNotify(ref _actualName, value);
+            }
+        }
+
+
+        public UserPageViewModel()
+        {
+            Name = "张三";
+            ActualName = "海盗";
+        }
+
+
+        public void ChangeName()
+        {
+            ActualName = Name;
+        }
+
+    }
+}
