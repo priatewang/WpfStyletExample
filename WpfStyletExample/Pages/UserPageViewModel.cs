ba64c8bac522ac0c306f9e539a5b4eb294410c1c
wangqiang <wqiang1956@126.com>
2023-04-11 15:57:45 +0800

    特殊用法测试

--- WpfStyletExample/Pages/UserPageViewModel.cs
+++ WpfStyletExample/Pages/UserPageViewModel.cs
@@ -9,6 +9,9 @@ namespace WpfStyletExample.Pages
 {
     public class UserPageViewModel : Screen
     {
+
+        #region 测试命令/事件
+
         private string _actualName;
 
         public string Name { get; set; }
@@ -25,6 +28,54 @@ namespace WpfStyletExample.Pages
         }
 
 
+        public void ChangeName()
+        {
+            ActualName = Name;
+        }
+
+        public void ChangingName()
+        {
+            ActualName = "改名ing";
+        }
+
+        public void ChangedName()
+        {
+            ActualName = "改名完成";
+        }
+        #endregion
+
+        #region 测试事件是否可用
+
+
+        private string _testTxt;
+
+        public string TestTxt
+        {
+            get
+            {
+                return _testTxt;
+            }
+            set
+            {
+                SetAndNotify(ref _testTxt, value);
+            }
+        }
+        private bool _canTestCmd;
+
+        public bool CanTestCmd
+        {
+            get
+            {
+                return _canTestCmd;
+            }
+            set
+            {
+                SetAndNotify(ref _canTestCmd, value);
+            }
+        }
+
+        #endregion
+
         public UserPageViewModel()
         {
             Name = "张三";
@@ -32,9 +83,32 @@ namespace WpfStyletExample.Pages
         }
 
 
-        public void ChangeName()
+        public void TestCmd()
         {
-            ActualName = Name;
+            TestTxt = "测试成功";
+        }
+
+        public void ChangeTestCmd()
+        {
+            CanTestCmd = !CanTestCmd;
+        }
+
+        private string _outText;
+        public string OutText
+        {
+            get
+            {
+                return _outText;
+            }
+            set
+            {
+                SetAndNotify(ref _outText, value);
+            }
+        }
+
+        public void OutUseTest()
+        {
+            OutText = "Shell操作修改成功";
         }
 
     }
